0e495dc42fbf24746a62c4d1a3afcf436ba22752
Jarrett-Blair <48661065+Jarrett-Blair@users.noreply.github.com>
2023-08-17 17:01:41 -0700

    Update dataset.py
    Customized dataset.py to my data

--- ct_classifier/dataset.py
+++ ct_classifier/dataset.py
@@ -13,8 +13,9 @@
 '''
 
 import os
-import json
+import pandas as pd
 from torch.utils.data import Dataset
+from sklearn.preprocessing import LabelEncoder
 from torchvision.transforms import Compose, Resize, ToTensor
 from PIL import Image
 
@@ -26,6 +27,7 @@ class CTDataset(Dataset):
             Constructor. Here, we collect and index the dataset inputs and
             labels.
         '''
+        # Where 
         self.data_root = cfg['data_root']
         self.split = split
         self.transform = Compose([              # Transforms. Here's where we could add data augmentation (see Björn's lecture on August 11).
@@ -33,33 +35,41 @@ class CTDataset(Dataset):
             ToTensor()                          # ...and convert them to torch.Tensor.
         ])
         
-        # index data into list
-        self.data = []
-
         # load annotation file
         annoPath = os.path.join(
             self.data_root,
-            'eccv_18_annotation_files',
-            'train_annotations.json' if self.split=='train' else 'cis_val_annotations.json'
+            cfg["annotate_root"],
+            'train.csv' if self.split=='train' else 'valid.csv'
         )
-        meta = json.load(open(annoPath, 'r'))
-
-        images = dict([[i['id'], i['file_name']] for i in meta['images']])          # image id to filename lookup
-        labels = dict([[c['id'], idx] for idx, c in enumerate(meta['categories'])]) # custom labelclass indices that start at zero
+        meta = pd.read_csv(annoPath)
         
-        # since we're doing classification, we're just taking the first annotation per image and drop the rest
-        images_covered = set()      # all those images for which we have already assigned a label
-        for anno in meta['annotations']:
-            imgID = anno['image_id']
-            if imgID in images_covered:
-                continue
+        class_labels = cfg['class_labels']
+        Y = meta[class_labels]
+        encoder = LabelEncoder()
+        encoder.fit(Y)
+        labelIndex = encoder.transform(Y)
+        
+        file_name = cfg['file_name']
+        imgFileName = meta[file_name]
+        
+        self.data = list(zip(imgFileName.tolist(), labelIndex.tolist()))
+
+        # images = dict([[i['id'], i['file_name']] for i in meta['images']])          # image id to filename lookup
+        # labels = dict([[c['id'], idx] for idx, c in enumerate(meta['categories'])]) # custom labelclass indices that start at zero
+        
+        # # since we're doing classification, we're just taking the first annotation per image and drop the rest
+        # images_covered = set()      # all those images for which we have already assigned a label
+        # for anno in meta['annotations']:
+        #     imgID = anno['image_id']
+        #     if imgID in images_covered:
+        #         continue
             
-            # append image-label tuple to data
-            imgFileName = images[imgID]
-            label = anno['category_id']
-            labelIndex = labels[label]
-            self.data.append([imgFileName, labelIndex])
-            images_covered.add(imgID)       # make sure image is only added once to dataset
+        #     # append image-label tuple to data
+        #     imgFileName = images[imgID]
+        #     label = anno['category_id']
+        #     labelIndex = labels[label]
+        #     self.data.append([imgFileName, labelIndex])
+        #     images_covered.add(imgID)       # make sure image is only added once to dataset
     
 
     def __len__(self):
@@ -77,10 +87,11 @@ class CTDataset(Dataset):
         image_name, label = self.data[idx]              # see line 57 above where we added these two items to the self.data list
 
         # load image
-        image_path = os.path.join(self.data_root, 'eccv_18_all_images_sm', image_name)
+        image_path = os.path.join(self.data_root, 'AllPhotosJPG', image_name)
         img = Image.open(image_path).convert('RGB')     # the ".convert" makes sure we always get three bands in Red, Green, Blue order
 
         # transform: see lines 31ff above where we define our transformations
         img_tensor = self.transform(img)
 
         return img_tensor, label
+
