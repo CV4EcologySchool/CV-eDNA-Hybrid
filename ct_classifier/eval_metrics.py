128ae0718b7c4983304ea8b12a5bc8d322b12ee5
Jarrett-Blair <48661065+Jarrett-Blair@users.noreply.github.com>
2023-08-21 15:47:22 -0700

    Update eval_metrics.py
    Added functions to make the code more modular

--- ct_classifier/eval_metrics.py
+++ ct_classifier/eval_metrics.py
@@ -14,54 +14,14 @@ import numpy as np
 import pandas as pd
 from tqdm import trange
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import seaborn as sns
 from train import create_dataloader, load_model       # NOTE: since we're using these functions across files, it could make sense to put them in e.g. a "util.py" script.
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report, roc_curve, precision_recall_curve, auc, confusion_matrix
 
-parser = argparse.ArgumentParser(description='Train deep learning model.')
-parser.add_argument('--config', help='Path to config file', default='../configs/exp_resnet18.yaml')
-args = parser.parse_args()
 
-# load config
-print(f'Using config "{args.config}"')
-cfg = yaml.safe_load(open(args.config, 'r'))
-
-
-# setup entities
-dl_test = create_dataloader(cfg, split='test')
-
-# load model
-model = load_model(cfg)
-
-data_root = cfg['data_root']
-
-# load annotation file
-annoPath = os.path.join(
-    data_root,
-    cfg["annotate_root"],
-    'valid.csv'
-)
-meta = pd.read_csv(annoPath)
-
-class_labels = cfg['class_labels']
-Y = meta[class_labels]
-Y = Y.unique()
-encoder = LabelEncoder()
-encoder.fit(Y)
-labelIndex = encoder.transform(Y)
-
-short_labels = cfg['short_labels']
-short_Y = meta[short_labels]
-short_Y = short_Y.unique()
-
-Y_ordered = sorted(Y)
-short_Y_ordered = [0] * len(short_Y)
-
-for i in labelIndex:
-    short_Y_ordered[labelIndex[i]] = short_Y[i]
-
-def evaluate(cfg, dataLoader, model):
+def predict(cfg, dataLoader, model):
     '''
         Validation function. Note that this looks almost the same as the training
         function, except that we don't use any optimizer or gradient steps.
@@ -111,68 +71,166 @@ def evaluate(cfg, dataLoader, model):
     
     return all_true, all_preds, all_probs
     
-all_true, all_preds, all_probs = evaluate(cfg, dl_test, model[0])
 
-report = classification_report(all_true, 
-                               all_preds, 
-                               target_names=short_Y_ordered,
-                               output_dict=True)
+def conf_table(conf_matrix, Y, prop = True):
+    """
+    Creates a confusion matrix as a pandas data frame pivot table
 
-report_path = os.path.join(
-    data_root,
-    'eval',
-    'metrics/eval.yaml')
-with open(report_path, 'w') as file:
-    yaml.dump(report, file)
+    Parameters:
+    - conf_matrix (Array): The standard confusion matrix output from sklearn
+    - Y (Array): The unique labels of the classifier. Will be used as conf_table labels
+    - prop (bool): Should the conf table use proportions (i.e. Recall) or total values?
 
-
-conf_matrix = confusion_matrix(all_true, all_preds)
-
-conf_data = []
-for i, row in enumerate(conf_matrix):
-    for j, count in enumerate(row):
-        conf_data.append([Y_ordered[i], Y_ordered[j], count])
-
-conf_df = pd.DataFrame(conf_data, columns=['Reference', 'Prediction', 'Count'])
-
-# Calculate proportions
-conf_df['prop'] = conf_df['Count'] / conf_df.groupby('Reference')['Count'].transform('sum')
-
-conf_table = conf_df.pivot_table(index='Reference', columns='Prediction', values='prop', fill_value=0)
-
-
-
-plt.figure(figsize=(16, 12))
-sns.set(font_scale=1)
-sns.set_style("white")
-
-ax = sns.heatmap(conf_table, cmap="coolwarm", cbar_kws={'label': 'Proportion'})
-
-# Customize the axis labels and ticks
-ax.set_xlabel("Predicted", fontsize=20)
-ax.set_ylabel("Actual", fontsize=20)
-ax.set_xticks(np.arange(len(short_Y_ordered)) + 0.5)
-ax.set_yticks(np.arange(len(short_Y_ordered)) + 0.5)
-ax.set_xticklabels(short_Y_ordered, fontsize=12)
-ax.set_yticklabels(short_Y_ordered, rotation=0, fontsize=12)
-
-# Add annotation
-ax.annotate("Predicted", xy=(0.5, -0.2), xytext=(0.5, -0.5), ha='center', va='center',
-             textcoords='axes fraction', arrowprops=dict(arrowstyle="-", lw=1))
+    Returns:
+    DataFrame: conf_table
+    """
+    
+    # Convert conf_matrix to list
+    conf_data = []
+    for i, row in enumerate(conf_matrix):
+        for j, count in enumerate(row):
+            conf_data.append([Y[i], Y[j], count])
+    
+    # Convert list to 
+    conf_df = pd.DataFrame(conf_data, columns=['Reference', 'Prediction', 'Count'])
+    
+    # If prop = True, calculate proportions
+    if prop:
+        conf_df['prop'] = conf_df['Count'] / conf_df.groupby('Reference')['Count'].transform('sum')
+        
+        # Create conf_table
+        conf_table = conf_df.pivot_table(index='Reference', columns='Prediction', values='prop', fill_value=0)
+        
+    else:
+        # Create conf_table
+        conf_table = conf_df.pivot_table(index='Reference', columns='Prediction', values='Count', fill_value=0)
+    
+    
+    return conf_table
 
 
-# Customize the appearance directly on the Axes object
-ax.set_xticklabels(short_Y_ordered, rotation=45, ha='right')
+def save_conf(cfg, table, Y):
+    """
+    Plots a confusion matrix
 
-# Save the plot
-conf_path = os.path.join(
-    data_root,
-    'eval',
-    'plots/conf.png')
+    Parameters:
+    - cfg (dict): The config yaml
+    - table (DataFrame): The conf_table
+    - Y (list): The class labels to be plotted on the conf_tab
 
-plt.savefig(conf_path)
+    Returns:
+    Saves plot to directory
+    """
+    
+    data_root = cfg['data_root']
+    
+    custom_gradient = ["#201547", "#00BCE1"]
+    n_bins = 100  # Number of bins for the gradient
 
-print("Done")
+    custom_cmap = LinearSegmentedColormap.from_list("CustomColormap", custom_gradient, N=n_bins)
 
+    plt.figure(figsize=(16, 12))
+    sns.set(font_scale=1)
+    sns.set_style("white")
+    
+    ax = sns.heatmap(table, cmap=custom_cmap, cbar_kws={'label': 'Proportion'})
+    
+    # Customize the axis labels and ticks
+    ax.set_xlabel("Predicted", fontsize=20)
+    ax.set_ylabel("Actual", fontsize=20)
+    ax.set_xticks(np.arange(len(Y)) + 0.5)
+    ax.set_yticks(np.arange(len(Y)) + 0.5)
+    ax.set_xticklabels(Y, fontsize=12)
+    ax.set_yticklabels(Y, rotation=0, fontsize=12)
+    
+    # Add annotation
+    ax.annotate("Predicted", xy=(0.5, -0.2), xytext=(0.5, -0.5), ha='center', va='center',
+                 textcoords='axes fraction', arrowprops=dict(arrowstyle="-", lw=1))
+    
+    
+    # Customize the appearance directly on the Axes object
+    ax.set_xticklabels(Y, rotation=45, ha='right')
+    
+    # Save the plot
+    conf_path = os.path.join(
+        data_root,
+        'eval',
+        'plots/conf.png')
+    
+    plt.savefig(conf_path)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Train deep learning model.')
+    parser.add_argument('--config', help='Path to config file', default='../configs/exp_resnet18.yaml')
+    args = parser.parse_args()
+    
+    # load config
+    print(f'Using config "{args.config}"')
+    cfg = yaml.safe_load(open(args.config, 'r'))
+    
+    
+    # setup entities
+    dl_test = create_dataloader(cfg, split='test')
+    
+    # load model
+    model = load_model(cfg)
+    
+    data_root = cfg['data_root']
+    
+    # load annotation file
+    annoPath = os.path.join(
+        data_root,
+        cfg["annotate_root"],
+        'valid.csv'
+    )
+    meta = pd.read_csv(annoPath)
+    
+    class_labels = cfg['class_labels']
+    Y = meta[class_labels]
+    Y = Y.unique()
+    encoder = LabelEncoder()
+    encoder.fit(Y)
+    labelIndex = encoder.transform(Y)
+    
+    short_labels = cfg['short_labels']
+    short_Y = meta[short_labels]
+    short_Y = short_Y.unique()
+    
+    Y_ordered = sorted(Y)
+    short_Y_ordered = [0] * len(short_Y)
+    
+    for i in labelIndex:
+        short_Y_ordered[labelIndex[i]] = short_Y[i]
+    
+    all_true, all_preds, all_probs = predict(cfg, dl_test, model[0])
+    
+    report = classification_report(all_true, 
+                                   all_preds, 
+                                   target_names=short_Y_ordered,
+                                   output_dict=True)
+
+    report_path = os.path.join(
+        data_root,
+        'eval',
+        'metrics/eval.yaml')
+    with open(report_path, 'w') as file:
+        yaml.dump(report, file)
+    
+    conf_matrix = confusion_matrix(all_true, all_preds)
+    
+    conf_tab = conf_table(conf_matrix, Y_ordered)
+    
+    save_conf(cfg, conf_tab, short_Y_ordered)
+
+    # That's all, folks!
+        
+
+
+if __name__ == '__main__':
+    # This block only gets executed if you call the "train.py" script directly
+    # (i.e., "python ct_classifier/train.py").
+    main()
 
 
