eea20fc365461aa36e8e5a4510b3463f168ee5ce
Jarrett-Blair <48661065+Jarrett-Blair@users.noreply.github.com>
2023-08-29 10:08:32 -0700

    Create get_taxa_events.R

---
+++ scripts/get_taxa_events.R
@@ -0,0 +1,15 @@
+library(dplyr)
+
+lktl_event_summary <- invert_cleanlab %>%
+  group_by(LKTL) %>%
+  summarize(Present_Events = paste(Event, collapse = ", ")) %>%
+  ungroup()
+
+species_site_dict <- lktl_event_summary %>%
+  mutate(Present_Events = strsplit(Present_Events, ", ")) %>%
+  pull(Present_Events) %>%
+  set_names(lktl_event_summary$LKTL)
+
+unique_lists <- sapply(species_site_dict, unique)
+taxa_events <- sapply(unique_lists, length)
+taxa_events = as.data.frame(taxa_events)
