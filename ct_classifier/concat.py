454f3198bc3429659422dc5ba075a9152731e888
Jarrett-Blair <blair@zoology.ubc.ca>
2023-08-31 19:37:51 +0000

    First attempt at concat

---
+++ ct_classifier/concat.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 10:09:08 2023
+
+@author: jarre
+"""
+
+import torch
+import torch.nn as nn
+from torchvision.models import resnet
+
+class TabularModel(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(TabularModel, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+        )
+        
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
+
+class CustomResNet18(nn.Module):
+
+    def __init__(self):
+        '''
+            Constructor of the model. Here, we initialize the model's
+            architecture (layers).
+        '''
+        super(CustomResNet18, self).__init__()
+
+        self.feature_extractor = resnet.resnet18(pretrained=True)       # "pretrained": use weights pre-trained on ImageNet
+
+        # replace the very last layer from the original, 1000-class output
+        # ImageNet to a new one that outputs num_classes
+        last_layer = self.feature_extractor.fc                          # tip: print(self.feature_extractor) to get info on how model is set up
+        in_features = last_layer.in_features                            # number of input dimensions to last (classifier) layer
+        self.feature_extractor.fc = nn.Identity()                       # discard last layer...
+
+    
+
+    def forward(self, x):
+        '''
+            Forward pass. Here, we define how to apply our model. It's basically
+            applying our modified ResNet-18 on the input tensor ("x") and then
+            apply the final classifier layer on the ResNet-18 output to get our
+            num_classes prediction.
+        '''
+        # x.size(): [B x 3 x W x H]
+        features = self.feature_extractor(x)    # features.size(): [B x 512 x W x H]
+
+        return features
+    
+# Concatenated Model
+class ConcatenateModel(nn.Module):
+    def __init__(self, tabular_model, resnet_model, num_classes):
+        super(ConcatenateModel, self).__init__()
+        self.tabular_model = tabular_model
+        self.resnet_model = resnet_model
+        self.fc = nn.Sequential(
+            nn.Linear(tabular_model.fc[0].out_features + resnet_model.feature_extractor.fc.in_features, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, num_classes),
+        )
+        
+    def forward(self, x_tabular, x_images):
+        x_tabular = self.tabular_model(x_tabular)
+        x_images = self.resnet_model(x_images)
+        x_combined = torch.cat((x_tabular, x_images), dim=1)
+        x_combined = self.fc(x_combined)
+        return x_combined
+
+
+
+    
---
+++ ct_classifier/dataset_concat.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 11:01:23 2023
+
+@author: jarre
+"""
+
+import os
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+from sklearn.preprocessing import LabelEncoder
+from torchvision.transforms import Compose, Resize, ToTensor
+from PIL import Image
+
+
+class CTDataset(Dataset):
+
+    def __init__(self, cfg, split='train'):
+        '''
+            Constructor. Here, we collect and index the dataset inputs and
+            labels.
+        '''
+        # Where 
+        self.data_root = cfg['data_root']
+        self.split = split
+        self.transform = Compose([              # Transforms. Here's where we could add data augmentation (see Björn's lecture on August 11).
+            Resize((cfg['image_size'])),        # For now, we just resize the images to the same dimensions...
+            ToTensor()                          # ...and convert them to torch.Tensor.
+        ])
+        
+        # load annotation file
+        annoPath = os.path.join(
+            self.data_root,
+            cfg["annotate_root"],
+            'train.csv' if self.split=='train' else 'valid.csv'
+        )
+        trainPath = os.path.join(
+            os.path.dirname(annoPath),
+            'train.csv'
+        )
+        meta = pd.read_csv(annoPath)
+        train  = pd.read_csv(trainPath)      
+        
+        data_cols = range(cfg['data_cols'][0], cfg['data_cols'][1])
+        
+        X = meta.iloc[:, data_cols]
+        # convert to numpy arrays
+        X = np.array(X)
+        
+        class_labels = cfg['class_labels']
+        Y_train = train[class_labels]
+        encoder = LabelEncoder()
+        encoder.fit(Y_train)
+        
+        Y = meta[class_labels]
+        labelIndex = encoder.transform(Y)
+        
+        file_name = cfg['file_name']
+        imgFileName = meta[file_name]
+        
+        self.data = list(zip(X.tolist(), imgFileName.tolist(), labelIndex.tolist()))
+
+        # images = dict([[i['id'], i['file_name']] for i in meta['images']])          # image id to filename lookup
+        # labels = dict([[c['id'], idx] for idx, c in enumerate(meta['categories'])]) # custom labelclass indices that start at zero
+        
+        # # since we're doing classification, we're just taking the first annotation per image and drop the rest
+        # images_covered = set()      # all those images for which we have already assigned a label
+        # for anno in meta['annotations']:
+        #     imgID = anno['image_id']
+        #     if imgID in images_covered:
+        #         continue
+            
+        #     # append image-label tuple to data
+        #     imgFileName = images[imgID]
+        #     label = anno['category_id']
+        #     labelIndex = labels[label]
+        #     self.data.append([imgFileName, labelIndex])
+        #     images_covered.add(imgID)       # make sure image is only added once to dataset
+    
+
+    def __len__(self):
+        '''
+            Returns the length of the dataset.
+        '''
+        return len(self.data)
+
+    
+    def __getitem__(self, idx):
+        '''
+            Returns a single data point at given idx.
+            Here's where we actually load the image.
+        '''
+        X, image_name, label = self.data[idx]              # see line 57 above where we added these two items to the self.data list
+
+        # load image
+        image_path = os.path.join(self.data_root, 'AllPhotosJPG', image_name)
+        img = Image.open(image_path).convert('RGB')     # the ".convert" makes sure we always get three bands in Red, Green, Blue order
+
+        # transform: see lines 31ff above where we define our transformations
+        img_tensor = self.transform(img)
+
+        return X, img_tensor, label
+
