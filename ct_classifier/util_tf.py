d70de68746cf2df5007ed44db23b06fbfab8f7b6
JarrettBlair <48661065+Jarrett-Blair@users.noreply.github.com>
2023-09-28 16:28:51 -0700

    Create util_tf.py

---
+++ ct_classifier/util_tf.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 12:42:36 2023
+
+@author: blair
+"""
+import os
+import random
+import numpy as np
+import tensorflow as tf
+
+def init_seed(seed):
+    
+    os.environ['PYTHONHASHSEED']=str(seed)
+    random.seed(seed)
+    #np.random.seed(seed)
+    tf.random.set_seed(seed)
+    
