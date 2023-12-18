42e1a0571b8464dc9227061aa05062566295b8a7
JarrettBlair <48661065+Jarrett-Blair@users.noreply.github.com>
2023-12-18 12:27:40 -0800

    Add batch training files

---
+++ ct_classifier/tf_trainall.bat
@@ -0,0 +1,27 @@
+
+@echo off
+
+REM for /L %%i in (0,1,4) do (
+    REM echo Running train.py seed %%i
+    REM python tf_train.py --seed %%i
+REM )
+
+for /L %%i in (0,1,4) do (
+    echo Running tf_train_concat.py seed %%i
+    python tf_train_concat.py --seed %%i
+)
+
+for /L %%i in (0,1,4) do (
+    echo Running tf_train_concat.py sim seed %%i
+    python tf_train_concat.py --config ../configs/exp_resnet18_37141_concat_sim.yaml --seed %%i
+)
+
+for /L %%i in (0,1,4) do (
+    echo Running tf_train_concat.py seed %%i
+    python tf_train_concat.py --config ../configs/exp_resnet18_37141_concat_noise.yaml --seed %%i
+)
+
+
+echo All Python scripts have been executed.
+pause
+../configs/exp_resnet18_37141_concat.yaml
