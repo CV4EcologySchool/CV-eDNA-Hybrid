3a40d7f9451bec78878b410d20efa3ba7b5249e4
JarrettBlair <48661065+Jarrett-Blair@users.noreply.github.com>
2024-07-09 04:41:28 -0700

    Create order level scripts

---
+++ ct_classifier/dna_pr_order.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 08:22:17 2024
+
+@author: blair
+"""
+
+#from skmultilearn.problem_transform import BinaryRelevance
+from sklearn.metrics import precision_score, recall_score, confusion_matrix
+import numpy as np
+import json
+import os
+
+# Sample data
+with open(r"C:\Carabid_Data\CV-eDNA\splits\order\image_multilab_order.json") as json_file:
+    image_multilab = json.load(json_file)
+with open(r"C:\Carabid_Data\CV-eDNA\splits\order\dna_multilab_order.json") as json_file:
+    dna_multilab = json.load(json_file)
+
+ground_truth = [value for value in image_multilab.values()]
+predicted_labels = [value for value in dna_multilab.values()]
+
+# Get unique labels
+all_labels = sorted(list(set(label for labels in ground_truth + predicted_labels for label in labels)))
+
+# Create label-to-index and index-to-label mappings
+label_to_index = {label: index for index, label in enumerate(all_labels)}
+index_to_label = {index: label for label, index in label_to_index.items()}
+
+# Convert labels to binary arrays
+def label_to_binary(label_list):
+    binary_array = np.zeros(len(all_labels))
+    for label in label_list:
+        binary_array[label_to_index[label]] = 1
+    return binary_array
+
+ground_truth_bin = np.array([label_to_binary(labels) for labels in ground_truth])
+predicted_labels_bin = np.array([label_to_binary(labels) for labels in predicted_labels])
+
+# Calculate precision and recall for each label
+precisions = []
+recalls = []
+specificities = []
+
+for label_index in range(len(all_labels)):
+    precision = precision_score(ground_truth_bin[:, label_index], 
+                                predicted_labels_bin[:, label_index], 
+                                zero_division = 1)
+    recall = recall_score(ground_truth_bin[:, label_index], 
+                          predicted_labels_bin[:, label_index])
+    
+    conf_matrix = confusion_matrix(ground_truth_bin[:, label_index], 
+                                   predicted_labels_bin[:, label_index])
+    TN = conf_matrix[0, 0]
+    FP = conf_matrix[0, 1]
+    specificity = TN / (TN + FP)
+    
+    precisions.append(precision)
+    recalls.append(recall)
+    specificities.append(specificity)
+
+# Calculate precision and recall for each label
+label_precision_recall = {}
+
+for label_index, label in index_to_label.items():
+    precision = precision_score(ground_truth_bin[:, label_index], 
+                                predicted_labels_bin[:, label_index],
+                                zero_division = 1)
+    recall = recall_score(ground_truth_bin[:, label_index], 
+                          predicted_labels_bin[:, label_index])
+    
+    conf_matrix = confusion_matrix(ground_truth_bin[:, label_index], 
+                                   predicted_labels_bin[:, label_index])
+    TN = conf_matrix[0, 0]
+    FP = conf_matrix[0, 1]
+    specificity = TN / (TN + FP)
+    
+    label_precision_recall[label] = {'precision': precision, 
+                                     'recall': recall,
+                                     'specificity': specificity}
+
+sum(precisions) / len(precisions)
+sum(recalls) / len(recalls)
+sum(specificities) / len(specificities)
+
+os.chdir(r"C:\Users\jarre\ownCloud\CV-eDNA\splits\order")
+json_filename = "dna_pr.json"
+with open(json_filename, "w") as json_file:
+    json.dump(label_precision_recall, json_file, indent=4)
---
+++ ct_classifier/order_concat_eval.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 07:32:19 2024
+
+@author: blair
+"""
+
+import os
+os.chdir(r"C:\Users\blair\OneDrive - UBC\CV-eDNA-Hybrid\ct_classifier")
+
+import yaml
+import copy
+import argparse
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+# from tqdm import trange
+# from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import seaborn as sns
+# import plotly.graph_objects as go
+# from eval_metrics import predict, hierarchy, hierarchy_pred, conf_table, plt_conf, save_conf_html
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix, top_k_accuracy_score
+from tf_loader_concat import CTDataset   # Leave this, it helps for some reason
+from util_order import hierarchy, hierarchy_pred, conf_table, plt_conf
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Train deep learning model.')
+    parser.add_argument('--config', help='Path to config file', default='../configs')
+    parser.add_argument('--exp', help='Experiment name', default='exp_order_fusion')
+    args = parser.parse_args()
+    
+    # load config
+    print(f'Using config "{args.config}"')
+    cfg_path = os.path.join(
+        args.config,
+        args.exp)
+    cfg = yaml.safe_load(open(cfg_path + ".yaml"))
+    
+    experiment = cfg['experiment_name']
+    data_root = cfg['data_root']
+    seed = cfg['seed']
+    
+    # setup entities
+    test_loader = CTDataset(cfg, split='valid')   
+    test_generator = test_loader.create_tf_dataset()
+      
+    # load annotation file
+    annoPath = os.path.join(
+        data_root,
+        cfg["annotate_root"],
+        'valid.csv'
+    )
+    
+    trainPath = os.path.join(
+        os.path.dirname(annoPath),
+        'train.csv'
+    )
+    train  = pd.read_csv(trainPath)      
+      
+    class_labels = cfg['class_labels']
+    Y = train[class_labels]
+    Y = Y.unique()
+    encoder = LabelEncoder()
+    encoder.fit(Y)
+    labelIndex = encoder.transform(Y)
+    
+    short_labels = cfg['short_labels']
+    short_Y = train[short_labels]
+    short_Y = short_Y.unique()
+    
+    Y_ordered = sorted(Y)
+    short_Y_ordered = [0] * len(short_Y)
+    
+    for i in labelIndex:
+        short_Y_ordered[labelIndex[i]] = short_Y[i]
+    
+    all_true = []
+    for idx, (data, labels) in enumerate(test_generator):
+
+        all_true.append(labels)
+    
+    all_true = tf.concat(all_true, axis=0)
+    all_true = tf.argmax(all_true, axis=1)
+    all_true = all_true.numpy()
+    
+    named_true_long = [Y_ordered[index] for index in all_true]
+    named_true_short = [Y_ordered[index] for index in all_true]
+      
+    
+    # load model
+    model = tf.keras.models.load_model(f'model_states\{experiment}\{experiment}_{seed[0]}.h5')
+    
+    probs = model.predict(test_generator)
+    predicted_classes = tf.argmax(probs, axis=1)
+    predicted_classes = predicted_classes.numpy()
+          
+    named_pred_long = [Y_ordered[index] for index in predicted_classes]
+    named_pred_short = [Y_ordered[index] for index in predicted_classes]
+    
+    top3_indices = np.argsort(probs, axis=1)[:, -3:]
+    t3_class = np.any(top3_indices == all_true[:, np.newaxis], axis=1)
+    t3_acc = np.mean(t3_class)
+    
+    report = classification_report(named_true_short,
+                          named_pred_short,
+                          output_dict=True,
+                          zero_division=1)
+    recall_values = [report[key]['recall'] for key in set(named_true_short)]
+    average_recall = sum(recall_values) / len(recall_values)
+            
+   
+    conf_matrix = confusion_matrix(named_true_long,
+                          named_pred_long,
+                          labels = Y_ordered)
+
+    conf_tab = conf_table(conf_matrix, Y_ordered)    
+    figure, n = plt_conf(conf_tab, short_Y_ordered, report)
+    # # Save the plot
+    # fig_path = os.path.join(
+    #     conf_path,
+    #     f'conf_{n}_{level}.png')
+           
+    # figure.savefig(fig_path)
+    
+    #save_conf_html(cfg, conf_tab, short_Y_ordered, report)
+
+
+
+if __name__ == '__main__':
+    # This block only gets executed if you call the "train.py" script directly
+    # (i.e., "python ct_classifier/train.py").
+    main()
+
+
